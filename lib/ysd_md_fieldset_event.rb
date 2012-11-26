d7b54badd7a2925c42ea5d842ad5eed352f6404e
yurak <yurak.sisa.dream@gmail.com>
2012-11-26 10:30:54 +0100

    event

--- lib/ysd_md_fieldset_event.rb
+++ lib/ysd_md_fieldset_event.rb
@@ -9,9 +9,8 @@ module FieldSet
 
     def self.included(model)
        model.property :event_date_start, DateTime, :field => 'event_date_time_start'
-       model.property :event_time_start, String, :field => 'event_time_start', :length => 5
        model.property :event_date_end, DateTime, :field => 'event_date_end'
-       model.property :event_time_end, String, :field => 'event_time_end', :length => 5
+       model.property :event_time_zone, String, :length => 6, :default => 'Z'
     end
 
   end # Event
