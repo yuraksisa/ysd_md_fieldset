0ffb4e949c1127a1ee2361c681b8d64295683c5a
yurak <yurak.sisa.dream@gmail.com>
2013-02-12 13:16:59 +0100

    profile and content refactoring to datamapper

--- lib/ysd_md_fieldset_event.rb
+++ lib/ysd_md_fieldset_event.rb
@@ -10,7 +10,7 @@ module FieldSet
     def self.included(model)
        model.property :event_date_start, DateTime, :field => 'event_date_time_start'
        model.property :event_date_end, DateTime, :field => 'event_date_end'
-       model.property :event_time_zone, String, :length => 6, :default => 'Z'
+       model.property :event_time_zone, String, :field => 'event_time_zone', :length => 6, :default => 'Z'
     end
 
   end # Event
